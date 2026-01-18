582be7ce6081631a7ad9220066da6d7e520f378e
moniem2020 <moniemghazal@gmail.com>
2026-01-18 13:06:32 +0200

    fix: update footer to client component for dynamic year display

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
-﻿import Link from 'next/link';
+﻿'use client';
+
+import Link from 'next/link';
 import Image from 'next/image';
 import { Linkedin, Github, Mail, MapPin, Phone } from 'lucide-react';
 
